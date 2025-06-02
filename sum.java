f634346e4918b61101589bf2fc1177d0660a484c
golusunirmala2004 <nirmalareddygolusu@gmail.com>
2025-06-02 14:36:11 +0400

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sum{
+public static void main(String[] args)
+{
+int [] a=new int[5];
+Scanner sc=new Scanner(System.in);
+for(int i=0;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+}
+int sum=0;
+for(int i=0;i<a.length;i++)
+{
+sum=sum+a[i];
+
+}
+System.out.println(sum);
+}
+}
