fa88f168149da4ba6ad5b876d341149c2d4290e2
golusunirmala2004 <nirmalareddygolusu@gmail.com>
2025-06-17 14:44:55 +0530

    Create prepoorder.java

---
+++ prepoorder.java
@@ -0,0 +1,85 @@
+class node{
+    int data;
+    node left;
+    node right;
+    node(int data)
+    {
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+class tree{
+    node root;
+    tree()
+    {
+        this.root=null;
+    }
+    void insert(int n)
+    {
+        node newnode= new node(n);
+        if(this.root==null) this.root=newnode;
+        else{
+            node curr=this.root;
+            while(true)
+            {
+                if(n>curr.data)
+                {
+                    if(curr.right==null)
+                    {
+                        curr.right=newnode;
+                        break;
+                    }
+                    else{
+                        curr=curr.right;
+                    }
+                }
+                else{
+                    if(curr.left==null)
+                    {
+                        curr.left=newnode;
+                        break;
+                    }
+                    else{
+                        curr=curr.left;
+                    }
+                }
+            
+            }
+        }
+    }
+    void preorder(node root)
+    {
+        if(root!=null)
+        {
+            
+            System.out.println(root.data);
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+    void postorder(node root)
+    {
+        if(root!=null)
+        {
+          
+            postorder(root.left);
+            postorder(root.right);
+            System.out.println(root.data);
+        }
+    }
+}
+
+
+public class prepoorder
+{
+    public static void main(String[] args){
+       tree t=new tree();
+       t.insert(3);
+       t.insert(5);
+       t.insert(1);
+       t.preorder(t.root);
+       t.postorder(t.root);
+        
+    }
+}
