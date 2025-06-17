bd5fb170bd7b99ce856f572ba64a8f0fc3df03e9
golusunirmala2004 <nirmalareddygolusu@gmail.com>
2025-06-17 14:32:44 +0530

    Create Upperandlowercases.java

---
+++ Upperandlowercases.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Upperandlowercases{
+  public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+	 String a=sc.nextLine();
+
+		 System.out.println(a.toUpperCase());
+		 System.out.println(a.toLowerCase());
+
+	
+
+	 }
+  }
+  	 
