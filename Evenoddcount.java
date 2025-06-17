bc2c2b6d1b7f70cc45513e86828b44d38642f40f
golusunirmala2004 <nirmalareddygolusu@gmail.com>
2025-06-17 14:16:51 +0530

    Create Evenoddcount.java

---
+++ Evenoddcount.java
@@ -0,0 +1,22 @@
+ import java.util.*;
+public class Evenoddcount{
+     public static void main(String[] args){
+       int [] a=new int[6];
+	   Scanner sc=new Scanner(System.in);
+	   for(int i=0;i<a.length;i++){
+	       a[i]=sc.nextInt();
+	   }
+	   int ecount=0;
+	   int ocount=0;
+	   for(int i=0;i<a.length;i++){
+	      if(a[i]%2==0){import
+		     ecount+=1;
+		  }
+		  else{
+		     ocount+=1;
+		  }
+	    }
+		System.out.println(ecount);
+		  System.out.println(ocount);
+	} 
+}
